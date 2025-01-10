0f46c17729c9e9bc213e4a92b38703720996f4a7
Burgess Leo <liuxp731@qq.com>
2025-01-10 22:53:39 +0800

    delete describetion in .zsh

--- auto-venv.plugin.zsh
+++ auto-venv.plugin.zsh
@@ -1,14 +1,11 @@
 # auto-activate-venv.plugin.zsh
 
-# 定义全局变量
 PROJECT_PATH=""
 CURRENT_PATH=""
 
-# 自动激活虚拟环境的函数
 auto_activate() {
   local NEW_PATH=$(pwd)
 
-  # 只有在路径改变时才进行检查
   if [[ "$NEW_PATH" != "$CURRENT_PATH" ]]; then
     CURRENT_PATH="$NEW_PATH"
 
@@ -30,7 +27,6 @@ auto_activate() {
   fi
 }
 
-# 将 auto_activate 函数绑定到 PROMPT_COMMAND，以便在每次显示提示符时调用它
 autoload -Uz add-zsh-hook
 add-zsh-hook precmd auto_activate
 
