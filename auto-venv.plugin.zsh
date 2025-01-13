aaf8b511c4b61a13d50f4bf8886c74dbce5c50ea
Burgess Leo <liuxp731@qq.com>
2025-01-13 11:32:39 +0800

    fix bug that jump to new python project dont activate venv

--- auto-venv.plugin.zsh
+++ auto-venv.plugin.zsh
@@ -9,18 +9,26 @@ auto_activate() {
   if [[ "$NEW_PATH" != "$CURRENT_PATH" ]]; then
     CURRENT_PATH="$NEW_PATH"
 
-    if [[ -z "$PROJECT_PATH" ]]; then
-      if [ -d ".venv" ]; then
-        source .venv/bin/activate
+    # 检查是否存在虚拟环境目录
+    if [[ -d ".venv" ]] || [[ -d "venv" ]]; then
+      # 如果当前没有激活任何项目，则激活新的虚拟环境
+      if [[ -z "$PROJECT_PATH" ]]; then
+        source .venv/bin/activate || source venv/bin/activate
         PROJECT_PATH="$CURRENT_PATH"
+      else
+        # 如果当前路径不在已激活项目路径下，停用旧环境并激活新环境
+        if [[ "$CURRENT_PATH" != "$PROJECT_PATH"* ]]; then
+          if [[ -n "$VIRTUAL_ENV" ]]; then
+            deactivate
+          fi
+          source .venv/bin/activate || source venv/bin/activate
+          PROJECT_PATH="$CURRENT_PATH"
+        fi
       fi
     else
-      if [[ "$CURRENT_PATH" == "$PROJECT_PATH"* ]]; then
-        return
-      else
-        if [[ -n "$VIRTUAL_ENV" ]]; then
-          deactivate
-        fi
+      # 如果当前没有虚拟环境，且之前有激活的环境，则停用它
+      if [[ -n "$VIRTUAL_ENV" ]]; then
+        deactivate
         PROJECT_PATH=""
       fi
     fi
