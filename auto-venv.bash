fe0cb0a0c3201be8683c2cbdec103a60e5239c96
Burgess Leo <liuxiaopeng731@gmail.com>
2025-03-17 17:21:33 +0800

    add auto-venv for bash

---
+++ auto-venv.bash
@@ -0,0 +1,34 @@
+# Auto-activate uv venv
+PROJECT_PATH=""
+CURRENT_PATH=""
+
+auto_activate() {
+  local NEW_PATH=$(pwd)
+
+  # 只有在路径改变时才进行检查
+  if [[ "$NEW_PATH" != "$CURRENT_PATH" ]]; then
+    CURRENT_PATH="$NEW_PATH"
+
+    if [[ -z "$PROJECT_PATH" ]]; then
+      # 检查是否存在 .venv 或 venv 目录
+      if [ -d ".venv" ]; then
+        source .venv/bin/activate
+        PROJECT_PATH="$CURRENT_PATH"
+      elif [ -d "venv" ]; then
+        source venv/bin/activate
+        PROJECT_PATH="$CURRENT_PATH"
+      fi
+    else
+      # 如果当前路径在项目路径下，则保持激活状态
+      if [[ "$CURRENT_PATH" == "$PROJECT_PATH"* ]]; then
+        return
+      else
+        # 如果不在项目路径下，则停用虚拟环境
+        if [[ -n "$VIRTUAL_ENV" ]]; then
+          deactivate
+        fi
+        PROJECT_PATH=""
+      fi
+    fi
+  fi
+}
